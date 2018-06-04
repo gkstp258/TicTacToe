e6ba526421d55064a976f13236f78c368ee1a6f0
GeonK <gksry3s@naver.com>
2018-06-04 20:21:38 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
+function Square(props) { //이긴 칸 배경색깔
+  const winningSquareStyle = {
+    backgroundColor: '#ccc'
+  };
+
+  return (	//이긴 칸의 배경색을 winningSquareStyle에서 가져온다
+    <button className="square" onClick={props.onClick} style={props.winningSquare ? winningSquareStyle : null}>
       {props.value}
     </button>
   );
@@ -12,11 +16,13 @@ function Square(props) {
 
 class Board extends React.Component {
   renderSquare(i, row, col) {
+    let winningSquare = this.props.winner && this.props.winner.includes(i) ? true : false; //승리자의 사각형을 표시해주기 위해 선언한 변수
     return (
       <Square
         key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i, row, col)}
+        winningSquare = {winningSquare}
       />
     );
   }
@@ -32,7 +38,7 @@ class Board extends React.Component {
       row = [];
       for(let j = 0; j < 3; j++) {
         row.push(this.renderSquare(num, i, j)); /* i(renderSquare의 매개변수) = num, row = i, col = j. num이 없으면 한칸 클릭시 한줄이 다 채
-						워지고 i or j가 없으면 안넣으면 각각 undefined가 뜬다 */
+	                                        워지고 i or j가 없으면 안넣으면 각각 undefined가 뜬다 */
         num++;  //칸하나를 선택할 때 전체 다 선택되지 않기 위해 1씩 증가시켜준다
       }
       squares.push(<div key ={num} className="board-row">{row}</div>);
@@ -55,7 +61,7 @@ class Game extends React.Component {
         }
       ],
       stepNumber: 0,
-			xIsNext: true,
+         xIsNext: true,
       clicked: null,  //(row,col)형식으로 바꾸기 위한 변수
       ascending: true  //오름차순 또는 내림차순으로 변경하기 위한 변수
     };
@@ -69,13 +75,13 @@ class Game extends React.Component {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-		squares[i] = this.state.xIsNext ? "X" : "O";
-		
+      squares[i] = this.state.xIsNext ? "X" : "O";
+      
     this.setState({
       history: history.concat([
         {
-					squares: squares,
-					clicked: [row,col]
+               squares: squares,
+               clicked: [row,col]
         }
       ]),
       stepNumber: history.length,
@@ -103,22 +109,22 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-			
-			let desc = 'Go to Game start';
-			let row = null;
-			let col = null;
-			
-			//만약 어떤 한 사각형을 선택하면 그 사각형의 움직임이 (row,col) 형식으로 뜬다
-			//예를 들면 처음에 플레이어가 첫번째 줄에서 3번째 칸에 있는 사각형을 선택했다면 Go to move: #1 (0,2) 라고 뜬다
-			if(move) {
-				desc = 'Go to move: #' +move;
-				row = '('+this.state.history[move].clicked[0]+', ';
-				col = this.state.history[move].clicked[1]+')';
-			}
+         
+         let desc = 'Go to Game start';
+         let row = null;
+         let col = null;
+         
+         //만약 어떤 한 사각형을 선택하면 그 사각형의 움직임이 (row,col) 형식으로 뜬다
+         //예를 들면 처음에 플레이어가 첫번째 줄에서 3번째 칸에 있는 사각형을 선택했다면 Go to move: #1 (0,2) 라고 뜬다
+         if(move) {
+            desc = 'Go to move: #' +move;
+            row = '('+this.state.history[move].clicked[0]+', ';
+            col = this.state.history[move].clicked[1]+')';
+         }
 
       //move list(if문)를 bold처리해줌
       //move와 stepNumber의 값이 완전히 일치하면 bold처리를 해줌
-			let bold = (move === this.state.stepNumber ? 'bold' : '');
+         let bold = (move === this.state.stepNumber ? 'bold' : '');
 
       return (
         <li key={move}>
@@ -133,8 +139,10 @@ class Game extends React.Component {
     }
 
     let status;
-    if (winner) {
-      status = "Winner: " + winner;
+    if (winner && winner !== 'draw') { //무승부가 아니라면 승자는 X 또는 O가 나옴
+      status = "Winner: " + (this.state.xIsNext ? "O" : "X");
+    } else if (winner && winner === 'draw'){  //무승부라면 Match is draw 출력
+      status = "Match is " + winner;
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
@@ -145,6 +153,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i, row, col) => this.handleClick(i, row, col)}
+            winner={winner && winner.winningSquares}
           />
         </div>
         <div className="game-info">
@@ -173,7 +182,9 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], winningSquares: lines[i]};
+    } else if(!squares.includes(null)) {	//사각형에 null이 없으면 9칸이 다찬것이므로 무승부
+      return 'draw';
     }
   }
   return null;
